6d0e4dfda571602ca8faf33c7591fd771515129b
atanasova16 <m.atanasovaa@yahoo.com>
2022-06-26 17:06:53 +0300

    Update pca_bg.R

--- pca_bg.R
+++ pca_bg.R
@@ -1,4 +1,4 @@
-bul <- readxl::read_xlsx("C:/Users/User/Documents/UNITN/Computational social science/bulgaria/final_cities.xlsx")
+bul <- readxl::read_xlsx("C:/Users/User/Documents/UNITN/Computational social science/bulgaria/final_CITIES.xlsx")
 
 bul$activity_rate <- as.double(bul$activity_rate)
 bul$labour_force_thousands <- as.double(bul$labour_force_thousands)
@@ -57,8 +57,8 @@ fviz_nbclust(bul_transform, kmeans, method = 'silhouette')
 fviz_nbclust(bul_transform, kmeans, method = 'gap_stat')
 
 
-k = 5
-kmeans_bul = kmeans(bul_transform, centers = k, nstart = 50)
+k = 4
+kmeans_bul = kmeans(bul_transform, centers = k, nstart = 20)
 fviz_cluster(kmeans_bul, data = bul_transform)
 
 
@@ -68,12 +68,59 @@ library(scales)
 palette(alpha(brewer.pal(9,'Set1'), 0.5))
 plot(comp, col=k$clust, pch=16)
 
-boxplot(bul$Population2021~ k$cluster,
-        xlab='Cluster', ylab='Accommodation',
-        main='Plane Accommodation by Cluster')
+clusters_kmeans4 <- k$cluster
 
-boxplot(bul$employment_rate~ k$cluster,
-        xlab='Cluster', ylab='Accommodation',
-        main='Plane Accommodation by Cluster')
-k$cluster
+
+bul_with_labels <- bul[,]
+bul_with_labels$kmeans4 <- as.factor(clusters_kmeans4)
+
+scaled <- as.data.frame(scale(bul_with_labels[,-c(1,30)]))
+scaled$Region <- bul$Region
+scaled$kmeans4 <- as.factor(clusters_kmeans4)
+
+
+library(randomForest)
+rf_bul <- randomForest(kmeans4 ~.-Region, data = scaled, mtry = 6, importance = TRUE)
+importance(rf_bul)
+varImpPlot(rf_bul)
+
+
+boxplot(bul_with_labels$connected_to_wastewater_collecting ~ bul_with_labels$kmeans4,
+        xlab='Cluster', ylab='connected_to_wastewater_collecting',
+        main='connected_to_wastewater_collecting by Cluster')
+boxplot(bul$employment_rate ~ bul_with_labels$kmeans4,
+        xlab='Cluster', ylab='employment_rate',
+        main='employment_rate by Cluster')
+
+#HCLUST WITHOUT PCA
+distmatrix <- dist(scaled[,-c(29,30)])
+hc <- hclust(distmatrix, method = "complete")
+plot(hc, labels = scaled$Region)
+labs_hc4 <- cutree(hc, 4)
+
+#HCLUST WITH PCA
+distmatrix2 <- dist(bul_transform)
+hc2 <- hclust(distmatrix2, method = "complete")
+plot(hc2, labels = scaled$Region)
+labs_pca_hc4 <- cutree(hc, 4)
+
+#Compare hclust with kmeans both with the usage of PCA
+#in hclust, we can change 1 and 2 to correspond to the kmeans
+i = labs_pca_hc4 == 1
+j = labs_pca_hc4 == 2
+labs_pca_hc4[i] = 2
+labs_pca_hc4[j] = 1
+labs_pca_hc4 == clusters_kmeans4
+sum(labs_pca_hc4 == clusters_kmeans4)
+table(labs_pca_hc4, clusters_kmeans4)
+bul_with_labels$labs_pca_hc4 <- as.factor(labs_pca_hc4)
+
+#To see which cities are classified differently
+bul_with_labels$Region[bul_with_labels$kmeans4 == 1 & bul_with_labels$labs_pca_hc4 == 2]
+
+#WHY???
+
+
+write.csv(bul_with_labels, "C:/Users/User/Documents/UNITN/Computational social science/bulgaria/bul_with_labels.csv")
+
 
